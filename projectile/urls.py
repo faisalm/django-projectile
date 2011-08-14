19467ed601302c3c4bde9416ec8e54f7df3cfb3c
Faisal Mahmud <mahmud.faizal@gmail.com>
2011-08-14 02:21:26 +0200

    Housekeeping of urls.py done

--- projectile/urls.py
+++ projectile/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('projectile.views',
-    (r'^$', 'index', {'template_name': 'front/index.html'}, 'home'),
-    (r'^projekt/$', 'projects', {'template_name': 'front/projects.html'}, 'projects'),
-    (r'^projekt/(?P<project_slug>[-\w]+)/$', 'project_detail', {'template_name': 'front/project-detail.html'}, 'project_detail'),
-    # regex, view path, {dict}, name
+    (r'^project/$', 'projects', {'template_name': 'front/projects.html'}, 'projects'),
+    (r'^project/(?P<project_slug>[-\w]+)/$', 'project_detail', {'template_name': 'front/project-detail.html'}, 'project_detail'),
 )
