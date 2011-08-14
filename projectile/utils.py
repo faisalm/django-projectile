b25ad205f1ce5b123b74694ecb6b4fcf0286cd28
Faisal Mahmud <mahmud.faizal@gmail.com>
2011-08-14 02:23:11 +0200

    Deleted some unused files and comments

--- projectile/utils.py
+++
@@ -1,8 +0,0 @@
-def get_string_as_ul(separator, string):
-    output = '<ul>\n'
-    for v in string.split(separator):
-        output += '\t<li>' + v.strip(' ') + '</li>\n'
-    output += '</ul>'
-    print output
-
-get_string_as_ul(',', 'HTML, CSS, JavaScript, Python Ista')
